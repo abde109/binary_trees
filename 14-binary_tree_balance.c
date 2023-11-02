146dbb4632c81830b27d0b3f5481b14595fc3052
abde109 <abderrahimkhadri2@gmail.com>
2023-11-02 23:10:49 +0100

    perfect

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,11 +11,12 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	int left_height = tree->left ? (int) binary_tree_height(tree->left) : -1;
-	int right_height = tree->right ? (int) binary_tree_height(tree->right) : -1;
+	int left_height = (tree->left) ? (int)binary_tree_height(tree->left) : -1;
+	int right_height = (tree->right) ? (int)binary_tree_height(tree->right) : -1;
 
 	return (left_height - right_height);
 }
+
 
 /**
  * binary_tree_height - Measures the height of a binary tree
@@ -25,12 +26,17 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		size_t left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		size_t right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
 
-		return ((left_height > right_height) ? left_height : right_height);
-	}
-	return (0);
+	size_t left_height = 0, right_height = 0;
+
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	return (left_height > right_height ? left_height : right_height);
 }
+
