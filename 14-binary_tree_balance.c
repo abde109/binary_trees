e9fa695ab4d2a3deda5f4c8dc6658f7f79ac7609
abde109 <abderrahimkhadri2@gmail.com>
2023-11-02 22:58:45 +0100

    is full

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,3 +16,21 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (left_height - right_height);
 }
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: A pointer to the node to measure the height from
+ *
+ * Return: The height of the tree starting at @tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		size_t right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+		return ((left_height > right_height) ? left_height : right_height);
+	}
+	return (0);
+}
